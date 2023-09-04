a4a0c634b92a83c8d5d5ce9df8c6f7753c97cc36
noeul1114 <parkhyongsok@naver.com>
2023-09-04 11:02:25 +0900

    db connect update

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,13 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import Registration
 
 
 def hello_world(request):
     if request.method == "POST":
-        temp = request.POST.get('email')
+        email = request.POST.get('email')
+
+        reg = Registration()
+        reg.email = email
+        reg.save()
+
+        reg_all = Registration.objects.all()
+
         return render(request, "accountapp/hello_world.html",
-                      context={"temp": temp})
+                      context={"temp": email,
+                               "reg_all": reg_all})
 
     temp = "techit"
     return render(request, "accountapp/hello_world.html",
