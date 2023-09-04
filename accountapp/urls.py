684905912e573179b0bb0d5f0ccf16a8b165cb67
noeul1114 <parkhyongsok@naver.com>
2023-09-04 14:17:29 +0900

    loginview update

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView
+from accountapp.views import hello_world, AccountCreateView, AccountLoginView
 
 app_name = "accountapp"
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path("hello_world/", hello_world, name="hello_world"),
 
     path("create/", AccountCreateView.as_view(), name="create"),
+    path("login/", AccountLoginView.as_view(), name="login"),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
@@ -35,6 +36,10 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("accountapp:hello_world")
     template_name = "accountapp/create.html"
+
+
+class AccountLoginView(LoginView):
+    template_name = "accountapp/login.html"
 
 
 
