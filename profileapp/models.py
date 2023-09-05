5d7587b37720ddf195ceb910f860dff524f96c8a
noeul1114 <parkhyongsok@naver.com>
2023-09-05 15:40:41 +0900

    profileapp model update

--- profileapp/models.py
+++ profileapp/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    nickname = models.CharField(max_length=100, null=False, unique=True)
+    image = models.ImageField(upload_to="profile/", null=True)
+    message = models.CharField(max_length=200, null=True)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='profile')
