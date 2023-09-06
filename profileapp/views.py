8bea3c386a4d4755653779566899d5083c2f028b
noeul1114 <parkhyongsok@naver.com>
2023-09-06 10:47:51 +0900

    profileapp createview update

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    form_class = ProfileForm
+    template_name = 'profileapp/create.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail',
+                       kwargs={'pk': self.kwargs['pk']})
+
