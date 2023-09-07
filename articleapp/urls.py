5b639b18f221bf5578ee07c92146ae2fb13762b7
noeul1114 <parkhyongsok@naver.com>
2023-09-07 11:43:48 +0900

    articleapp tempview update

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,7 +1,11 @@
+from django.urls import path
 
+from articleapp.views import TempView
 
-app_name = 'aricleapp'
+app_name = 'articleapp'
 
 urlpatterns = [
+
+    path('temp/', TempView.as_view(), name="temp"),
 
 ]
