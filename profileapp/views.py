aea8914424ff1560a5c17ce88393964190d18cdd
noeul1114 <parkhyongsok@naver.com>
2023-09-07 11:15:14 +0900

    profileapp decorator update

--- profileapp/views.py
+++ profileapp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileForm
 from profileapp.models import Profile
 
@@ -22,7 +24,8 @@ class ProfileCreateView(CreateView):
                        kwargs={'pk': self.request.user.pk})
 
 
-
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     form_class = ProfileForm
