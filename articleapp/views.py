a23b46e02508fd87a5d5152cd5b407898b607fde
noeul1114 <parkhyongsok@naver.com>
2023-09-12 11:14:38 +0900

    commentapp Mixin update

--- articleapp/views.py
+++ articleapp/views.py
@@ -5,10 +5,12 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView, CreateView, DetailView, UpdateView, DeleteView, ListView
+from django.views.generic.edit import FormMixin
 
 from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleForm
 from articleapp.models import Article
+from commentapp.forms import CommentCreationForm
 
 
 class TempView(TemplateView):
@@ -39,8 +41,9 @@ class ArticleCreateView(CreateView):
                        kwargs={'pk': self.object.pk})
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(DetailView, FormMixin):
     model = Article
+    form_class = CommentCreationForm
     context_object_name = 'target_article'
     template_name = 'articleapp/detail.html'
 
