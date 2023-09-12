c0b8cd93c8d4eb57adfcf24741647877079326d3
noeul1114 <parkhyongsok@naver.com>
2023-09-12 14:08:22 +0900

    commentapp deleteview update

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from commentapp.views import CommentCreateView, CommentUpdateView
+from commentapp.views import CommentCreateView, CommentUpdateView, CommentDeleteView
 
 app_name = 'commentapp'
 
@@ -8,5 +8,6 @@ urlpatterns = [
 
     path('create/', CommentCreateView.as_view(), name='create'),
     path('update/<int:pk>/', CommentUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', CommentDeleteView.as_view(), name='delete'),
 
 ]
--- commentapp/views.py
+++ commentapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, DeleteView
 
 from commentapp.decorators import comment_ownership_required
 from commentapp.forms import CommentCreationForm
@@ -36,6 +36,19 @@ class CommentUpdateView(UpdateView):
     def get_success_url(self):
         return reverse('articleapp:detail',
                        kwargs={'pk': self.object.article.pk})
+
+
+@method_decorator(comment_ownership_required, 'get')
+@method_decorator(comment_ownership_required, 'post')
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail',
+                       kwargs={'pk': self.object.article.pk})
+
 
 
 
