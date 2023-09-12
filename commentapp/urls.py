beef61aa5b406fd524ba3e123533c8de754a1156
noeul1114 <parkhyongsok@naver.com>
2023-09-12 13:44:16 +0900

    commentapp updateview update

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from commentapp.views import CommentCreateView
+from commentapp.views import CommentCreateView, CommentUpdateView
 
 app_name = 'commentapp'
 
 urlpatterns = [
 
     path('create/', CommentCreateView.as_view(), name='create'),
+    path('update/<int:pk>/', CommentUpdateView.as_view(), name='update'),
 
 ]
--- commentapp/views.py
+++ commentapp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
 
+from commentapp.decorators import comment_ownership_required
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
 
@@ -21,4 +23,23 @@ class CommentCreateView(CreateView):
     def get_success_url(self):
         return reverse('articleapp:detail',
                        kwargs={'pk': self.object.article.pk})
+
+
+@method_decorator(comment_ownership_required, 'get')
+@method_decorator(comment_ownership_required, 'post')
+class CommentUpdateView(UpdateView):
+    model = Comment
+    form_class = CommentCreationForm
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/update.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail',
+                       kwargs={'pk': self.object.article.pk})
+
+
+
+
+
+
 
