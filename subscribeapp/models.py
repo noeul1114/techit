6f8c198b9387ee4a8eb6d0c0fa4a963772b385e4
noeul1114 <parkhyongsok@naver.com>
2023-09-13 11:25:11 +0900

    subscribeapp  model update

--- subscribeapp/models.py
+++ subscribeapp/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False,
+                             related_name='owner')
+    target_user = models.ForeignKey(User, on_delete=models.CASCADE, null=False,
+                                    related_name='target')
+
+    class Meta:
+        unique_together = ('user', 'target_user')
+
+
--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -3,5 +3,5 @@
 app_name = 'subscribeapp'
 
 urlpatterns = [
-        
+
 ]
