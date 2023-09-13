5c8165211c7a4313c8741052a18cfda5c684864d
noeul1114 <parkhyongsok@naver.com>
2023-09-13 14:18:26 +0900

    subscribeapp listview update

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from subscribeapp.views import SubscribeView
+from subscribeapp.views import SubscribeView, SubscriptionListView
 
 app_name = 'subscribeapp'
 
 urlpatterns = [
 
     path('sub/<int:pk>/', SubscribeView.as_view(), name='sub'),
+
+    path('list/', SubscriptionListView.as_view(), name='list'),
 
 ]
--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView
 
+from articleapp.models import Article
 from subscribeapp.models import Subscription
 
 
@@ -27,6 +28,17 @@ class SubscribeView(RedirectView):
         return reverse('accountapp:detail',
                        kwargs={'pk': self.kwargs['pk']})
 
+
+class SubscriptionListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'subcribeapp/list.html'
+    paginate_by = 20
+
+    def get_queryset(self):
+        target_user_list = Subscription.objects.filter(user=self.request.user).values_list('target_user')
+        article_list = Article.objects.filter(writer__in=target_user_list)
+        return article_list
 
 
 
