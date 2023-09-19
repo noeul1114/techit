be5721b1a24dc49910d40ecfb2aaa9bcbe0e86fe
noeul1114 <parkhyongsok@naver.com>
2023-09-19 10:51:37 +0900

    settings update

---
+++ techit/settings/deploy.py
@@ -0,0 +1,26 @@
+from .base import *
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-1nr6-589u1-%893*qp)uewlkat$h0m$1q+)#&+tf)_0-bis7&m'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    '*'
+]
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
