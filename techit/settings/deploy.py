a7346ef36cd5e684330e590d98d6689dd6f61aa2
noeul1114 <parkhyongsok@naver.com>
2023-09-19 11:14:12 +0900

    settings update

--- techit/settings/deploy.py
+++ techit/settings/deploy.py
@@ -19,8 +19,12 @@ ALLOWED_HOSTS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "mariadb",
+        "USER": "django",
+        "PASSWORD": "password1234",
+        "HOST": "mariadb",
+        "PORT": "3306",
     }
 }
